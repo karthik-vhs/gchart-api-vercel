2659d6b7cc51f688a622efa3ebe375a54d6caf6e
karthik-vhs <karthik@vhsconsulting.net>
2025-08-22 18:01:43 +0530

    rm config

--- api/index.js
+++ api/index.js
@@ -1,10 +1,4 @@
 import serverless from 'serverless-http';
 import app from '../lib/app.js';
 
-// Vercel function config
-export const config = {
-  runtime: 'nodejs20.x',
-  regions: ['bom1'] // optional (Mumbai)
-};
-
 export default serverless(app);
