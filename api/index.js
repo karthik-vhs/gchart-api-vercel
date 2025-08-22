e3a61d9ad4b716873f1cc028480a7a705db3d041
karthik-vhs <karthik@vhsconsulting.net>
2025-08-22 18:17:20 +0530

    504 GATEWAY_TIMEOUT

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import serverless from 'serverless-http';
 import app from '../lib/app.js';
 
+// Remove explicit runtime version (new Vercel requires just 'nodejs' or none)
 export default serverless(app);
